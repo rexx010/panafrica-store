a6017a7b5938ae9303a25ba47d1c07b7d9d97071
Adeiga Oluwadamilare <grexx200@gmail.com>
2026-04-16 13:49:56 +0100

    Added rate-limiting middleware for login and checkout endpoints with IP and user-based restrictions

---
+++ src/middleware/rateLimit.middleware.js
@@ -0,0 +1,33 @@
+const rateLimit = require('express-rate-limit');
+
+const loginLimitChecker = rateLimit({
+    windowMs: 60 * 1000,
+    max: 10,
+    keyGenerator: (req) => req.ip,
+    handler: (req, res) => {
+        return res.status(429).json({
+            success: false,
+            error: true,
+            message: 'Too many login attemps. Please try again in a minute',
+        });
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+const checkoutLimitChecker = rateLimit({
+    windowMs: 60 * 1000,
+    max: 5,
+    keyGenerator: (req) => req.user.id,
+    handler: (req, res) => {
+        return res.status(429).json({
+            success: false,
+            error: true,
+            message: 'Too many checkout attempt. Please try again in a minute',
+        });
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+module.exports = { loginLimitChecker, checkoutLimitChecker }
