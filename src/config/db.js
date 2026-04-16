5918331d27613ebed7093d0558605da309bf4300
Adeiga Oluwadamilare <grexx200@gmail.com>
2026-04-16 13:47:49 +0100

    Added database connection setup with PostgreSQL using environment configuration and SSL support

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+const {Pool} = require('pg');
+
+require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+
+    ssl: {rejectUnauthorized: false},
+});
+
+pool.connect((err, client, release) => {
+    if(err) {
+        console.error('Database connection failed:', err.message);
+    } else{
+        console.log('Database connected successfully');
+        release();
+    }
+})
+
+module.exports = pool;
