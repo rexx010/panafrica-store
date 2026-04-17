b731f7b31cc1a00111e507df55531a1be92867a5
Adeiga Oluwadamilare <grexx200@gmail.com>
2026-04-17 03:31:36 +0100

    debugging mode

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-// TEMP DEBUG MIDDLEWARE - remove after fix
 app.use((req, res, next) => {
     console.log(`==> ${req.method} ${req.path}`);
     next();
@@ -35,7 +34,6 @@ app.use('/products', productsRoutes);
 app.use('/cart', cartRoutes);
 app.use('/', ordersRoutes);
 
-// FIXED error handler - was all on one line before
 app.use((err, req, res, next) => {
     logger.error('Unhandled error reached global handler', err);
     res.status(err.statusCode || 500).json({
@@ -63,5 +61,10 @@ const { startRateRefreshJob } = require('./modules/rates/rates.service');
 startRateRefreshJob().catch((err) => {
     console.error('Failed to start rate refresh job:', err.message);
 });
+
+const https = require('https');
+setInterval(() => {
+    https.get('https://panafrica-store.onrender.com/health', () => {}).on('error', () => {});
+}, 14 * 60 * 1000); 
 
 module.exports = app;
